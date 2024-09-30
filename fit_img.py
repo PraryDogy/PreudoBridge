176fc7c13adc74fa89069e3dc74ad428f587e9a8
Evgeny Loshkarev <evlosh@gmail.com>
2024-09-30 17:07:58 +0300

    ref

--- fit_img.py
+++ fit_img.py
@@ -1,35 +1,7 @@
 import cv2
-from PIL import Image
 import numpy as np
 
-class FitImg:
-    # @staticmethod
-    # def fit(img: Image.Image, w: int, h: int) -> Image.Image:
-    #     imw, imh = img.size
-
-    #     if -3 < imw - imh < 3:
-    #         imw, imh = imw, imw
-
-    #     if w > h:
-
-    #         if imw > imh:
-    #             delta = imw/imh
-    #             neww, newh = w, int(w/delta)
-    #         else:
-    #             delta = imh/imw
-    #             neww, newh = int(h/delta), h
-        
-    #     else:
-
-    #         if imw > imh:
-    #             delta = imw/imh
-    #             neww, newh = w, int(w/delta)
-    #         else:
-    #             delta = imh/imw
-    #             neww, newh = int(h/delta), h
-
-    #     return img.resize((neww, newh))
-    
+class FitImg:    
     @staticmethod
     def start(image: np.ndarray, size: int) -> np.ndarray | None:
         try:
