c75f400cf13180bb8687e2341308833754789939
Morkik <evlosh@gmail.com>
2024-10-01 22:24:42 +0300

    ref

--- widgets/grid_search.py
+++ widgets/grid_search.py
@@ -3,11 +3,15 @@ import os
 import sqlalchemy
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QGridLayout, QWidget, QLabel
 
 from cfg import Config
 from database import Cache, Dbase
 from fit_img import FitImg
 from utils import Utils
+
+from .grid_base import GridBase
+
 
 class SearchFinderThread(QThread):
     finished = pyqtSignal()
@@ -72,12 +76,37 @@ class SearchFinderThread(QThread):
                         else:
                             pixmap = QPixmap("images/file_210.png")
 
-
-                    self.new_widget.emit(
-                        {
-                            "src": src,
-                            "pixmap": pixmap
-                            }
-                            )
-
+                    self.new_widget.emit({"src": src, "pixmap": pixmap})
         self.session.commit()
+
+
+class GridSearch(GridBase):
+    def __init__(self, width: int, search_text: str):
+        super().__init__()
+        self.setWidgetResizable(True)
+
+        Config.img_viewer_images.clear()
+
+        self.clmn_count = width // Config.thumb_size
+        if self.clmn_count < 1:
+            self.clmn_count = 1
+
+        self.row, self.col = 0, 0
+
+        main_wid = QWidget()
+        self.grid_layout = QGridLayout(main_wid)
+        self.grid_layout.setSpacing(5)
+        self.setWidget(main_wid)
+
+        self.test = SearchFinderThread(Config.json_data["root"], search_text)
+        self.test.new_widget.connect(self.add_new_widget)
+
+    def add_new_widget(self, data: dict):
+        widget = QLabel()
+        widget.setPixmap(data["widget"])
+        self.grid_layout.addWidget(widget)
+
+        self.col += 1
+        if self.col >= self.clmn_count:
+            self.col = 0
+            self.row += 1
