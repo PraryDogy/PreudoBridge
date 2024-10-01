30a9c5f8ffab5d7b6f446f92e230243bcb7d93b3
Morkik <evlosh@gmail.com>
2024-10-01 21:23:12 +0300

    ref

--- widgets/top_bar.py
+++ widgets/top_bar.py
@@ -1,12 +1,96 @@
+import os
 import subprocess
+from difflib import SequenceMatcher
 
-from PyQt5.QtCore import QTimer, pyqtSignal
+from PyQt5.QtCore import QThread, QTimer, pyqtSignal
 from PyQt5.QtWidgets import (QFrame, QGridLayout, QLabel, QLineEdit, QMenu,
                              QPushButton, QSizePolicy, QSpacerItem,
                              QVBoxLayout, QWidget)
 
 from cfg import Config
-from path_finder import PathFinderThread
+
+
+class PathFinderThread(QThread):
+    finished = pyqtSignal(str)
+
+    def __init__(self, src: str):
+        super().__init__()
+        self.src: str = src
+        self.result: str = None
+        self.volumes: list = []
+
+    def run(self):
+        self.path_finder()
+        if not self.result:
+            self.finished.emit("")
+        elif self.result in self.volumes:
+            self.finished.emit("")
+        elif self.result:
+            self.finished.emit(self.result)
+
+    def path_finder(self):
+        src = os.sep + self.src.replace("\\", os.sep).strip().strip(os.sep)
+        src_splited = [i for i in src.split(os.sep) if i]
+
+        self.volumes = [
+            os.path.join("/Volumes", i)
+            for i in os.listdir("/Volumes")
+            ]
+
+        volumes_extra = [
+            os.path.join(vol, *extra.strip().split(os.sep))
+            for extra in Config.json_data["extra_paths"]
+            for vol in self.volumes
+            ]
+        
+        self.volumes.extend(volumes_extra)
+
+        # обрезаем входящий путь каждый раз на 1 секцию с конца
+        cut_paths: list = [
+                os.path.join(*src_splited[:i])
+                for i in range(len(src_splited) + 1)
+                if src_splited[:i]
+                ]
+
+        # обрезаем каждый путь на 1 секцию с начала и прибавляем элементы из volumes
+        all_posible_paths: list = []
+
+        for p_path in sorted(cut_paths, key=len, reverse=True):
+            p_path_split = [i for i in p_path.split(os.sep) if i]
+            
+            for share in self.volumes:
+                for i in range(len(p_path_split) + 1):
+
+                    all_posible_paths.append(
+                        os.path.join(share, *p_path_split[i:])
+                        )
+
+        # из всех полученных возможных путей ищем самый подходящий существующий путь
+        for i in sorted(all_posible_paths, key=len, reverse=True):
+            if os.path.exists(i):
+                self.result = i
+                break
+
+        # смотрим совпадает ли последняя секция входящего и полученного пути
+        tail = []
+
+        if self.result:
+            result_tail = self.result.split(os.sep)[-1]
+            if src_splited[-1] != result_tail:
+                try:
+                    tail = src_splited[src_splited.index(result_tail) + 1:]
+                except ValueError:
+                    return
+
+        # пытаемся найти секции пути, написанные с ошибкой
+        for a in tail:
+            dirs = [x for x in os.listdir(self.result)]
+
+            for b in dirs:
+                matcher = SequenceMatcher(None, a, b).ratio()
+                if matcher >= 0.85:
+                    self.result = os.path.join(self.result, b)
+                    break
 
 
 class SortTypeWidget(QPushButton):
