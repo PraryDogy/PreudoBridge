c9f4c1f53521ba64f5e3cf1cac8f557c3113a140
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-02 10:49:10 +0300

    ref

--- gui.py
+++ gui.py
@@ -2,10 +2,9 @@ import json
 import os
 import subprocess
 
-from PyQt5.QtCore import QDir, QEvent, QObject, Qt, QTimer, pyqtSignal
+from PyQt5.QtCore import QDir, QEvent, QObject, Qt, QTimer
 from PyQt5.QtGui import QCloseEvent, QKeyEvent, QResizeEvent
-from PyQt5.QtWidgets import (QAction, QApplication, QFileSystemModel, QLabel,
-                             QMenu, QSplitter, QTabWidget, QTreeView,
+from PyQt5.QtWidgets import (QApplication, QLabel, QSplitter, QTabWidget,
                              QVBoxLayout, QWidget)
 
 from cfg import Config
@@ -14,71 +13,6 @@ from widgets.grid_search import GridSearch
 from widgets.grid_standart import GridStandart
 from widgets.top_bar import TopBar
 from widgets.tree_folders import TreeFolders
-
-
-class TreeFolders(QTreeView):
-    folders_tree_clicked = pyqtSignal(str)
-
-    def __init__(self):
-        super().__init__()
-
-        self.model = QFileSystemModel()
-        self.model.setFilter(QDir.AllDirs | QDir.NoDotAndDotDot)
-        self.model.setRootPath("/Volumes")
-        self.setModel(self.model)
-        self.setRootIndex(self.model.index("/Volumes"))
-
-        self.setHeaderHidden(True)
-        for i in range(1, self.model.columnCount()):
-            self.setColumnHidden(i, True)
-
-        self.setIndentation(10)
-        self.setUniformRowHeights(True)
-
-        self.clicked.connect(self.one_clicked)
-
-    def one_clicked(self, index):
-        path = self.model.filePath(index)
-        self.setCurrentIndex(index)
-        self.folders_tree_clicked.emit(path)
-
-        if self.isExpanded(index):
-            self.collapse(index)
-        else:
-            self.expand(index)
-
-    def expand_path(self, root: str):
-        index = self.model.index(root)
-        self.setCurrentIndex(index)
-        self.expand(index)
-
-    def contextMenuEvent(self, event):
-        index = self.indexAt(event.pos())
-        if not index.isValid():
-            return
-
-        menu = QMenu(self)
-        file_path = self.model.filePath(index)
-        index = self.model.index(file_path)
-
-        open_finder_action = QAction("Просмотр", self)
-        open_finder_action.triggered.connect(lambda: self.one_clicked(index))
-        menu.addAction(open_finder_action)
-
-        menu.addSeparator()
-
-        open_finder_action = QAction("Показать в Finder", self)
-        open_finder_action.triggered.connect(lambda: self.open_in_finder(file_path))
-        menu.addAction(open_finder_action)
-
-        copy_path_action = QAction("Скопировать путь до папки", self)
-        copy_path_action.triggered.connect(lambda: Utils.copy_path(file_path))
-        menu.addAction(copy_path_action)
-
-        menu.exec_(self.mapToGlobal(event.pos()))
-
-    def open_in_finder(self, path: str):
-        subprocess.call(["open", "-R", path])
 
 
 class SimpleFileExplorer(QWidget):
