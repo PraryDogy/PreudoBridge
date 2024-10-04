0f27f3ee8347e03a60df3ad66211d44287acead6
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-04 17:51:06 +0300

    ref

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
+import cv2
 import os
+import rawpy
+
+src = "/Users/Loshkarev/Desktop/RAW"
+
+for img in os.listdir(src):
+    img = os.path.join(src, img)
+    print("start read image", img)
+
+    read = rawpy.imread(img)
+    read = read.postprocess()
 
 
+
+    cv2.imshow("23", read)
+    cv2.waitKey(0)
+
+
+# rawpy
