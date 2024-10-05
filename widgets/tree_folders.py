2952b1d925324e57b74cf8f005927dd0e7ca7a18
Morkik <evlosh@gmail.com>
2024-10-05 15:37:39 +0300

    ref

--- widgets/tree_folders.py
+++ widgets/tree_folders.py
@@ -2,81 +2,59 @@ import os
 import subprocess
 
 from PyQt5.QtCore import QDir, QModelIndex, pyqtSignal, Qt
-from PyQt5.QtGui import QContextMenuEvent
 from PyQt5.QtWidgets import (QAction, QFileSystemModel, QLabel, QListView,
-                             QListWidget, QListWidgetItem, QMenu, QVBoxLayout,
-                             QWidget)
+                             QMenu, QTreeView)
 
 from cfg import Config
 from utils import Utils
 
 
-class TreeFolders(QWidget):
+class TreeFolders(QTreeView):
     folders_tree_clicked = pyqtSignal(str)
     add_to_favs_clicked = pyqtSignal(str)
     del_favs_clicked = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
-        
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(0)
-        self.setLayout(layout)
-
-        self.back_btn = QListWidget()
-        self.back_btn.setSelectionMode(QListWidget.SelectionMode.NoSelection)
-        self.back_btn.setFixedHeight(27)
-        self.add_item()
-        layout.addWidget(self.back_btn)
 
         self.c_model = QFileSystemModel()
         self.c_model.setFilter(QDir.AllDirs | QDir.NoDotAndDotDot)
-        self.c_model.setRootPath('/Volumes')
+        self.c_model.setRootPath("/Volumes")
+        self.setModel(self.c_model)
+        self.setRootIndex(self.c_model.index("/Volumes"))
 
-        self.list_view = QListView()
-        self.list_view.setModel(self.c_model)
-        self.list_view.setRootIndex(self.c_model.index("/Volumes"))
-        layout.addWidget(self.list_view)
+        self.setHeaderHidden(True)
+        for i in range(1, self.c_model.columnCount()):
+            self.setColumnHidden(i, True)
 
-        self.list_view.doubleClicked.connect(self.on_double_click)
-        self.list_view.contextMenuEvent = self.custom_context
+        self.setIndentation(10)
+        self.setUniformRowHeights(True)
 
-    def add_item(self):
-        wid = QLabel(text="Назад")
-        wid.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        wid.setFixedHeight(25)
-        list_item = QListWidgetItem()
-        list_item.setSizeHint(wid.sizeHint())
+        self.clicked.connect(self.one_clicked)
 
-        self.back_btn.addItem(list_item)
-        self.back_btn.setItemWidget(list_item, wid)
-
-        wid.mouseDoubleClickEvent = self.back_click
-
-    def back_click(self, e):
-        root = os.path.dirname(Config.json_data["root"])
-        self.expand_path(root)
-        self.folders_tree_clicked.emit(root)
-
-    def on_double_click(self, index: QModelIndex):
+    def one_clicked(self, index):
         path = self.c_model.filePath(index)
-        self.expand_path(path)
+        self.setCurrentIndex(index)
         self.folders_tree_clicked.emit(path)
-    
-    def expand_path(self, path: str):
-        self.list_view.setRootIndex(self.c_model.index(path))
 
-    def custom_context(self, event: QContextMenuEvent) -> None:
-        index = self.list_view.indexAt(event.pos())
+        if self.isExpanded(index):
+            self.collapse(index)
+        else:
+            self.expand(index)
+
+    def expand_path(self, root: str):
+        index = self.c_model.index(root)
+        self.setCurrentIndex(index)
+        self.expand(index)
+
+    def contextMenuEvent(self, event):
+        index = self.indexAt(event.pos())
         if not index.isValid():
             return
 
-        # Получаем глобальную позицию меню
-        global_pos = self.list_view.mapToGlobal(event.pos())
-
         menu = QMenu(self)
         src = self.c_model.filePath(index)
+        index = self.c_model.index(src)
 
         open_finder_action = QAction("Просмотр", self)
         open_finder_action.triggered.connect(lambda: self.one_clicked(index))
@@ -104,8 +82,7 @@ class TreeFolders(QWidget):
             fav_action.triggered.connect(lambda: self.add_to_favs_clicked.emit(src))
             menu.addAction(fav_action)
 
-        # Показываем меню в правильной глобальной позиции
-        menu.exec_(global_pos)
+        menu.exec_(self.mapToGlobal(event.pos()))
 
     def open_in_finder(self, path: str):
         subprocess.call(["open", "-R", path])
