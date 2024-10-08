7623fba8f46ff3200035b94c8b37670d5ba2e714
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-08 10:17:40 +0300

    ref

--- widgets/win_img_view.py
+++ widgets/win_img_view.py
@@ -281,6 +281,7 @@ class WinImgView(QWidget):
 
     def load_image_thread(self):
         self.setWindowTitle("Загрузка")
+        self.image_label.clear()
         img_thread = LoadImageThread(self.img_src)
         Shared.threads.append(img_thread)
         img_thread.finished.connect(
