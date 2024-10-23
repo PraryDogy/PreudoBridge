0c8ba78ce73b15f3df970b3b16b9e8a49bef3367
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-23 16:45:06 +0300

    ref

--- signals.py
+++ signals.py
@@ -3,6 +3,8 @@ from PyQt5.QtCore import QObject, pyqtSignal
 class Signals(QObject):
     load_standart_grid = pyqtSignal(object)
     resize_grid = pyqtSignal(object)
+    add_fav = pyqtSignal(object)
+    del_fav = pyqtSignal(object)
 
     def __init__(self) -> None:
         super().__init__()
