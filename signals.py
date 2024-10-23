8f083564ff7afea40c4b355db6c692e775b16002
Morkik <evlosh@gmail.com>
2024-10-23 23:22:11 +0300

    ref

--- signals.py
+++ signals.py
@@ -1,12 +1,16 @@
+from typing import Any
 from PyQt5.QtCore import QObject, pyqtSignal
+
 
 class Signals(QObject):
     load_standart_grid = pyqtSignal(object)
     load_search_grid = pyqtSignal(object)
 
+    # эти сигналы переназначаются заново, не забудь отключить прежде
     resize_grid = pyqtSignal(object)
     sort_grid = pyqtSignal(object)
     filter_grid = pyqtSignal(object)
+    move_to_wid = pyqtSignal(object)
 
     add_fav = pyqtSignal(object)
     del_fav = pyqtSignal(object)
@@ -15,7 +19,6 @@ class Signals(QObject):
     show_in_folder = pyqtSignal(object)
     progressbar_value = pyqtSignal(object)
     open_path = pyqtSignal(object)
-    move_to_wid = pyqtSignal(object)
 
     def __init__(self) -> None:
         super().__init__()
