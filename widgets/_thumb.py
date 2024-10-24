b92d01e2d57f2b0715c4de39027d395758d824ba
Evgeny Loshkarev <evlosh@gmail.com>
2024-10-24 15:56:54 +0300

    куа

--- widgets/_thumb.py
+++ widgets/_thumb.py
@@ -22,54 +22,37 @@ class NameLabel(QLabel):
         super().__init__()
 
     def update_name(self, rating: int, colors: str, name: str) -> list[str]:
-        # максимальная длина строки исходя из ширины pixmap в Thumb
+        # Максимальная длина строки исходя из ширины pixmap в Thumb
         max_row = TEXT_LENGTH[JsonData.pixmap_size_ind]
-        name_lines = []
-
+        
+        # Разбиение имени на строки в зависимости от длины
         if len(name) > max_row:
-            # если есть цветные теги то имя файла в 1 строку
             if colors:
-                # разрезаем имя
-                # было: very_long_name.jpg
-                # стало: very_lo...ame.jpg
-                name_start = name[:max_row - 10]
-                name_end = name[-7:]
-                name = [name_start + "..." + name_end]
-            
-            # если нет цветных тегов то имя файла может быть в 2 строки
+                # Если есть цветные теги, имя в одну строку
+                name = [f"{name[:max_row - 10]}...{name[-7:]}"]
             else:
+                # Иначе имя может быть в 2 строки
                 first_part = name[:max_row]
                 second_part = name[max_row:]
-
+                
                 if len(second_part) > max_row:
-                    # разрезаем вторую строку
-                    # было: very_long_name.jpg
-                    # стало: very_lo...ame.jpg
-                    name_start = second_part[:max_row - 10]
-                    name_end = second_part[-7:]
-                    second_part = name_start + "..." + name_end
-
+                    second_part = f"{second_part[:max_row - 10]}...{second_part[-7:]}"
+                
                 name = [first_part, second_part]
-
-        # Добавляем текстовые строки
-        if isinstance(name, str):
-            name_lines.append(name)
         else:
-            name_lines.extend(name)
+            name = [name]
 
         if rating > 0:
-            name_lines.append("\U00002605" * rating)
-        else:
-            name_lines.append("")
-
+            name.append("\U00002605" * rating)
+        
         if colors:
-            name_lines.append(colors)
-        else:
-            if len(name_lines) == 2:
-                name_lines.append("")
+            name.append(colors)
 
-        name_lines = sorted(name_lines, key=lambda x: len(x), reverse=1)
-        self.setText("\n".join(name_lines))
+        while len(name) < 3:
+            name.append("")
+
+        name = sorted(name, key=len, reverse=True)
+        self.setText("\n".join(name))
 
 
 class Thumb(QFrame):
