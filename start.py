c9a2a05636b4ea782542c98ec36d7ed869ac763a
Evgeny Loshkarev <evlosh@gmail.com>
2024-12-04 14:45:40 +0300

    ref

--- start.py
+++ start.py
@@ -22,7 +22,8 @@ class System_:
     def set_plugin_path(cls) -> bool:
         #lib folder appears when we pack this project to .app with py2app
         if os.path.exists("lib"): 
-            plugin_path = "lib/python3.11/PyQt5/Qt5/plugins"
+            ver = f"{sys.version_info.major}.{sys.version_info.minor}"
+            plugin_path = f"lib/python{ver}/PyQt5/Qt5/plugins"
             os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
             return True
         else:
