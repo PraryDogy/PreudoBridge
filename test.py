6633b2e3d11f2b87a9d3897ea35d1285228454e7
Evgeny Loshkarev <evlosh@gmail.com>
2024-12-05 11:59:45 +0300

    куа

--- test.py
+++ test.py
@@ -1,11 +1,53 @@
-from PIL import Image
+import os
+
 import numpy as np
-import cv2
+from PIL import Image
+import psd_tools
+import time
+import logging
 
-src = "/Volumes/Shares/Studio/MIUZ/Photo/Art/Ready/1 Solo/1 IMG/R01-SOL171-025-G3.psd"
+psd_tools.psd.tagged_blocks.warn = lambda *args, **kwargs: None
+psd_logger = logging.getLogger("psd_tools")
+psd_logger.setLevel(logging.CRITICAL)
 
-img = Image.open(src)
-img = np.array(img)
-img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-cv2.imshow("1", img)
-cv2.waitKey(0)
+src = "/Users/Loshkarev/Desktop/PSD"
+
+images = [
+    os.path.join(src, i)
+    for i in os.listdir(src)
+]
+
+
+def psd_tools_time(images):
+    start = time.time()
+
+    for i in images:
+
+        img = psd_tools.PSDImage.open(fp=i)
+        img = img.composite()
+        img = np.array(img)
+
+    end = time.time() - start
+    end = round(end, 2)
+
+    return end
+
+
+def PIL_time(images):
+    start = time.time()
+
+    for i in images:
+        img = Image.open(i)
+        img = np.array(img)
+
+    end = time.time() - start
+    end = round(end, 2)
+
+    return end
+
+
+a = psd_tools_time(images)
+b = PIL_time(images)
+
+print(a)
+print(b)
