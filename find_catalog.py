51ace3ab84a25a07904d188bb2cb043ae3bd72bb
Evgeny Loshkarev <evlosh@gmail.com>
2025-01-28 13:57:14 +0300

    ref

--- find_catalog.py
+++ find_catalog.py
@@ -30,50 +30,51 @@ def find_and_copy_files():
     input("Скопируйте список файлов (по одному имени в строке) и нажмите Enter...")
     text = read_clipboard()
     
-    src_files = [
-        i
-        for i in text.split('\n')
-        if i
-        ]
+    src_filenames: list[str] = []
 
-    src_files_lower = [
-        i.lower()
-        for i in src_files
-        ]
+    for i in text.split("\n"):
+
+        filename = i.strip()
+        filename, _ = os.path.splitext(filename)
+
+        if filename:
+            src_filenames.append(filename)
+
+        
+    src_lower_filenames = [i.lower() for i in src_filenames]
     
-    if src_files:
+    if src_lower_filenames:
         print("Поиск")
     
     else:
         print("Нет списка файлов")
         return
-    
+        
     for root, _, files in os.walk(src_folder):
         for full_filename in files:
 
             filename, _ = os.path.splitext(full_filename)
             filename_lower = filename.lower()
 
-            if filename_lower in src_files_lower:
+            if filename_lower in src_lower_filenames:
 
                 source_path = os.path.join(root, full_filename)
                 shutil.copy(source_path, dest_folder)
                 print(f"Copied: {full_filename}")
 
-    dest_files_lower = [
-        i.lower()
-        for i in os.listdir(dest_folder)
-        if os.path.isfile(i)
-    ]
+    dest_files_lower = []
+
+    for i in os.listdir(dest_folder):
+        filename, _ = os.path.splitext(i)
+        filename_lower = filename.lower()
+        dest_files_lower.append(filename_lower)
 
     miss_files = []
 
-    for i in src_files:
+    for i in src_filenames:
 
-        filename, ext = os.path.splitext(i)
         filename_lower = filename.lower()
-
-        if filename_lower.lower() not in dest_files_lower:
+        if filename_lower not in dest_files_lower:
             miss_files.append(str(i))
     
     if miss_files:
@@ -82,14 +83,4 @@ def find_and_copy_files():
         print("\n".join(miss_files))
 
 
-# find_and_copy_files()
-
-
-src = "/Users/Loshkarev/Desktop/Новая папка"
-
-files = [
-    i
-    for i in os.listdir(src)
-]
-
-print(files)
+find_and_copy_files()
