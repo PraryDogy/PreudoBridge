26f64f966d1d10c7e0cf506a05d3bcfea83350fd
Morkik <evlosh@gmail.com>
2025-02-16 12:00:14 +0300

    куа

---
+++ widgets/grid_search copy.py
@@ -0,0 +1,265 @@
+import os
+import traceback
+from difflib import SequenceMatcher
+
+from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtGui import QCloseEvent, QPixmap
+from sqlalchemy.exc import IntegrityError, OperationalError
+
+from cfg import JsonData, Static
+from database import Dbase, OrderItem
+from signals import SignalsApp
+from utils import URunnable, UThreadPool, Utils
+
+from ._grid import Grid, ThumbSearch
+from ._grid_tools import GridTools
+
+SLEEP = 0.2
+SQL_ERRORS = (IntegrityError, OperationalError)
+
+
+class WorkerSignals(QObject):
+    new_widget = pyqtSignal(OrderItem)
+    finished_ = pyqtSignal()
+
+
+class SearchFinder(URunnable):
+    def __init__(self, search_text: str):
+        super().__init__()
+
+        self.signals_ = WorkerSignals()
+        self.search_text: str = str(search_text)
+        self.extensions: tuple = None
+
+        self.db_path: str = None
+        self.conn = None
+
+    @URunnable.set_running_state
+    def run(self):
+        try:
+            self.setup_text()
+            self.scandir_main()
+
+            if self.should_run:
+
+                SignalsApp.instance.set_search_title.emit(
+                    self.search_text
+                )
+
+            self.signals_.finished_.emit()
+
+        except RuntimeError as e:
+            Utils.print_error(parent=None, error=e)
+
+    def setup_text(self):
+        extensions = Static.SEARCH_TEMPLATES.get(self.search_text)
+
+        if extensions:
+            self.extensions = extensions
+            self.process_entry = self.process_extensions
+
+        elif self.search_text == Static.SEARCH_LIST_TEXT:
+            self.process_entry = self.process_list
+
+        else:
+            self.process_entry = self.process_text
+
+    def word_similarity(self, word1: str, word2: str) -> float:
+        return SequenceMatcher(None, word1, word2).ratio()
+
+    def process_entry(self, entry: os.DirEntry): ...
+
+    def process_extensions(self, entry: os.DirEntry):
+        path = entry.path
+        path: str = path.lower()
+        if path.endswith(self.extensions):
+            return True
+        else:
+            return False
+
+    def process_text(self, entry: os.DirEntry):
+        filename, _ = os.path.splitext(entry.name)
+        filename: str = filename.lower()
+        search_text: str = self.search_text.lower()
+
+        if search_text in filename:
+            return True
+        else:
+            return False
+        
+    def process_list(self, entry: os.DirEntry):
+        filename, _ = os.path.splitext(entry.name)
+        filename: str = filename.lower()
+
+        if filename in Static.SEARCH_LIST:
+            return True
+        else:
+            return False
+
+    def scandir_main(self):
+
+        stack = [JsonData.root]
+
+        while stack:
+            current_dir = stack.pop()
+
+            if not self.should_run:
+                return
+            
+            try:
+                self.scan_current_dir(
+                    current_dir=current_dir,
+                    stack=stack
+                )
+
+            except Exception as e:
+                continue
+
+    def scan_current_dir(self, current_dir, stack: list):
+
+        with os.scandir(current_dir) as entries:
+            for entry in entries:
+
+                if not self.should_run:
+                    return
+
+                if entry.is_dir():
+                    stack.append(entry.path)
+                    continue
+
+                if self.process_entry(entry=entry):
+                    self.process_img(entry=entry)
+
+    def process_img(self, entry: os.DirEntry):
+        
+        stat = entry.stat()
+
+        order_item = OrderItem(
+            src=entry.path,
+            size=stat.st_size,
+            mod=stat.st_mtime,
+            rating=0
+        )
+
+        root = os.path.dirname(entry.path)
+        new_db_path = os.path.join(root, Static.DB_FILENAME)
+
+        # сравниваем предыдущий путь к БД и новый путь к БД
+        # если пути совпадают, значит мы находимся в прошлой директории
+        # и не нужно создавать новое подключение к БД
+
+        if new_db_path != self.db_path:
+
+            # создаем движок для БД в новой директории
+            dbase = Dbase()
+            engine = dbase.create_engine(path=new_db_path)
+
+            if engine is None:
+                self.signals_.new_widget.emit(order_item)
+                return
+            
+            self.db_path = new_db_path
+
+            # закрываем старое соединение
+            if self.conn:
+                self.conn.close()
+
+            # создаем новое соединение
+            self.conn = engine.connect()
+
+        try:
+
+            new_order_item = GridTools.update_order_item(
+                conn=self.conn,
+                order_item=order_item
+            )
+
+            if new_order_item:
+                self.signals_.new_widget.emit(new_order_item)
+
+        except Exception as e:
+            Utils.print_error(parent=self, error=e)
+            self.signals_.new_widget.emit(order_item)
+
+
+class GridSearch(Grid):
+    def __init__(self, width: int, search_text: str):
+        super().__init__(width)
+        self.setAcceptDrops(False)
+
+        self.col_count = Utils.get_clmn_count(width)
+        self.row, self.col = 0, 0
+        self.total = 0
+
+        SignalsApp.instance.bar_bottom_cmd.emit(
+            {"src": JsonData.root, "total": str(self.total)}
+        )
+
+        ThumbSearch.calculate_size()
+
+        self.task_ = SearchFinder(search_text)
+        self.task_.signals_.new_widget.connect(self.add_new_widget)
+        self.task_.signals_.finished_.connect(self.search_fin)
+        UThreadPool.start(self.task_)
+
+    def add_new_widget(self, order_item: OrderItem):
+        wid = ThumbSearch(
+            src=order_item.src,
+            size=order_item.size,
+            mod=order_item.mod,
+            rating=order_item.rating,
+            )
+        
+        if isinstance(order_item.pixmap_, QPixmap):
+            wid.set_pixmap(order_item.pixmap_)
+
+        wid.clicked_.connect(
+            lambda w=wid: self.select_one_wid(wid=w)
+        )
+
+        wid.control_clicked.connect(
+            lambda w=wid: self.control_clicked(wid=w)
+        )
+
+        wid.shift_clicked.connect(
+            lambda w=wid: self.shift_clicked(wid=w)
+        )
+
+        wid.r_clicked.connect(
+            lambda w=wid: self.context_thumb(wid=w)
+        )
+
+        wid.open_in_view.connect(
+            lambda w=wid: self.open_in_view(wid=w)
+        )
+
+        wid.mouse_moved.connect(
+            lambda w=wid: self.drag_thumb(wid=w)
+        )
+
+        self.add_widget_data(
+            wid=wid,
+            row=self.row,
+            col=self.col
+        )
+
+        self.grid_layout.addWidget(wid, self.row, self.col)
+
+        self.total += 1
+        self.col += 1
+        if self.col >= self.col_count:
+            self.col = 0
+            self.row += 1
+ 
+        if self.total % 2 == 0:
+            SignalsApp.instance.bar_bottom_cmd.emit(
+                {"total": str(self.total)}
+            )
+
+    def search_fin(self):
+        SignalsApp.instance.bar_bottom_cmd.emit(
+            {"total": str(self.total)}
+        )
+
+    def closeEvent(self, a0: QCloseEvent | None) -> None:
+        self.task_.should_run = False
---
+++ widgets/grid_standart copy.py
@@ -0,0 +1,304 @@
+import os
+
+import sqlalchemy
+from PyQt5.QtCore import QObject, Qt, pyqtSignal
+from PyQt5.QtGui import QCloseEvent, QPixmap
+from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtWidgets import QLabel
+from sqlalchemy.exc import IntegrityError, OperationalError
+
+from cfg import Dynamic, JsonData, Static
+from database import CACHE, Dbase, OrderItem
+from signals import SignalsApp
+from utils import URunnable, UThreadPool, Utils
+
+from ._finder_items import FinderItems, LoadingWid
+from ._grid import Grid, Thumb, ThumbFolder
+from ._grid_tools import GridTools
+
+WARN_TEXT = "Нет изображений или нет подключения к диску"
+TASK_NAME = "LOAD_IMAGES"
+SQL_ERRORS = (IntegrityError, OperationalError)
+
+
+class WorkerSignals(QObject):
+    new_widget = pyqtSignal(OrderItem)
+
+
+class LoadImages(URunnable):
+    def __init__(self, order_items: list[OrderItem]):
+        super().__init__()
+
+        self.signals_ = WorkerSignals()
+        self.order_items = [
+            i
+            for i in order_items
+            # if i.type_ != Static.FOLDER_TYPE
+        ]
+
+    @URunnable.set_running_state
+    def run(self):
+
+        # чтобы не создавать пустую ДБ в пустых или папочных директориях
+
+        if not self.order_items:
+            return
+
+        db = os.path.join(JsonData.root, Static.DB_FILENAME)
+        self.dbase = Dbase()
+        engine = self.dbase.create_engine(path=db)
+
+        if engine is None:
+            return
+
+        self.conn = engine.connect()
+
+        self.process_order_items()
+        self.process_removed_items()
+
+        self.conn.close()
+
+    def process_order_items(self):
+
+        for order_item in self.order_items:
+
+            if not self.should_run:
+                return
+            
+            try:
+
+                new_order_item = GridTools.update_order_item(
+                    conn=self.conn,
+                    order_item=order_item
+                )
+
+                if new_order_item:
+                    self.signals_.new_widget.emit(new_order_item)
+
+            except RuntimeError:
+                return
+
+            except Exception as e:
+                Utils.print_error(parent=self, error=e)
+                continue
+
+    def process_removed_items(self):
+
+        try:
+            Dynamic.busy_db = True
+            q = sqlalchemy.select(CACHE.c.id, CACHE.c.name)
+            res = self.conn.execute(q).fetchall()
+            Dynamic.busy_db = False
+        except SQL_ERRORS as e:
+            Utils.print_error(parent=self, error=e)
+            
+            return
+
+        order_items = [
+            Utils.hash_filename(filename=i.name)
+            for i in self.order_items
+        ]
+
+        del_items: list[int] = []
+
+        for id, name in res:
+            if name not in order_items:
+                del_items.append(id)
+
+        for id_ in del_items:
+
+            if not self.should_run:
+                return
+
+            q = sqlalchemy.delete(CACHE)
+            q = q.where(CACHE.c.id == id_)
+
+            try:
+                Dynamic.busy_db = True
+                self.conn.execute(q)
+                Dynamic.busy_db = False
+
+            except SQL_ERRORS as e:
+                Utils.print_error(parent=self, error=e)
+                self.conn.rollback()
+                continue
+
+        try:
+            Dynamic.busy_db = True
+            self.conn.commit()
+            Dynamic.busy_db = False
+
+        except SQL_ERRORS as e:
+            Utils.print_error(parent=self, error=e)
+            self.conn.rollback()
+
+
+class GridStandart(Grid):
+    def __init__(self, width: int, prev_path: str = None):
+
+        super().__init__(
+            width=width,
+            prev_path=prev_path
+        )
+
+        self.order_items: list[OrderItem] = []
+        self.tasks: list[LoadImages] = []
+
+        self.offset = 0
+        self.limit = 100
+
+        self.loading_lbl = LoadingWid(parent=self)
+        Utils.center_win(self, self.loading_lbl)
+        self.show()
+
+        self.finder_task = FinderItems()
+        self.finder_task.signals_.finished_.connect(self.finder_task_fin)
+        UThreadPool.start(self.finder_task)
+
+        self.verticalScrollBar().valueChanged.connect(self.on_scroll)
+
+    def on_scroll(self, value: int):
+
+        if value == self.verticalScrollBar().maximum():
+
+            if self.offset > self.total:
+                return
+            else:
+                self.offset += self.limit
+                self.create_sorted_grid()
+
+    def finder_task_fin(self, order_items: list[OrderItem]):
+
+        self.loading_lbl.hide()
+        self.order_items = order_items
+        self.total = len(order_items)
+
+        SignalsApp.instance.bar_bottom_cmd.emit(
+            {"src": JsonData.root, "total": self.total}
+        )
+
+        self.create_sorted_grid()
+
+    def create_sorted_grid(self):
+
+        sys_disk = os.path.join(os.sep, "Volumes", "Macintosh HD")
+        col_count = Utils.get_clmn_count(self.ww)
+        row, col = 0, 0
+
+        Thumb.calculate_size()
+
+        cut = self.order_items[self.offset:self.offset + self.limit]
+
+        for order_item in cut:
+
+            if os.path.isdir(order_item.src):
+
+                wid = ThumbFolder(
+                    src=order_item.src,
+                    size=order_item.size,
+                    mod=order_item.mod,
+                    rating=order_item.rating,
+                    )
+
+                if os.path.ismount(order_item.src) or order_item.src == sys_disk:
+                    img_wid = wid.img_frame.findChild(QSvgWidget)
+                    img_wid.load(Static.HDD_SVG)
+
+
+            else:
+                wid = Thumb(
+                    src=order_item.src,
+                    size=order_item.size,
+                    mod=order_item.mod,
+                    rating=order_item.rating,
+                    )
+
+            wid.clicked_.connect(
+                lambda w=wid: self.select_one_wid(wid=w)
+            )
+        
+            wid.control_clicked.connect(
+                lambda w=wid: self.control_clicked(wid=w)
+            )
+
+            wid.shift_clicked.connect(
+                lambda w=wid: self.shift_clicked(wid=w)
+            )
+
+            wid.r_clicked.connect(
+                lambda w=wid: self.context_thumb(wid=w)
+            )
+
+            wid.open_in_view.connect(
+                lambda w=wid: self.open_in_view(wid=w)
+            )
+
+            wid.mouse_moved.connect(
+                lambda w=wid: self.drag_thumb(wid=w)
+            )
+
+            self.add_widget_data(
+                wid=wid,
+                row=row,
+                col=col
+            )
+
+            self.grid_layout.addWidget(wid, row, col)
+
+            col += 1
+            if col >= col_count:
+                col = 0
+                row += 1
+
+        if self.cell_to_wid:
+            self.start_load_images(cut)
+
+        elif not os.path.exists(JsonData.root):
+            setattr(self, "no_images", WARN_TEXT)
+
+        elif not self.cell_to_wid:
+            setattr(self, "no_images", WARN_TEXT)
+
+        if hasattr(self, "no_images"):
+            no_images = QLabel(text=getattr(self, "no_images"))
+            no_images.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.grid_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.grid_layout.addWidget(no_images, 0, 0)
+
+        self.order_()
+        self.select_after_list()
+        
+    def start_load_images(self, cut_order_items: list[OrderItem]):
+        self.load_images_task_ = LoadImages(order_items=cut_order_items)
+        self.load_images_task_.set_name(text=TASK_NAME)
+        cmd_ = lambda image_data: self.set_pixmap(image_data)
+        self.load_images_task_.signals_.new_widget.connect(cmd_)
+        UThreadPool.start(self.load_images_task_)
+    
+    def set_pixmap(self, order_item: OrderItem):
+        try:
+
+            widget = Thumb.path_to_wid.get(order_item.src)
+
+            if isinstance(widget, Thumb):
+
+                if isinstance(order_item.pixmap_, QPixmap):
+                    widget.set_pixmap(pixmap=order_item.pixmap_)
+
+                if isinstance(order_item.rating, int):
+                    widget.set_rating(rating=order_item.rating)
+
+        except RuntimeError:
+            ...
+
+    def closeEvent(self, a0: QCloseEvent | None) -> None:
+        
+        for task in UThreadPool.current:
+            if task.get_name() == TASK_NAME:
+                task.should_run = False
+
+        return super().closeEvent(a0)
+
+    def resizeEvent(self, a0):
+        Utils.center_win(self, self.loading_lbl)
+        return super().resizeEvent(a0)
