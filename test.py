b16f7bab3c4fc49bc77aa62fee8c924c288ed0ac
Evgeny Loshkarev <evlosh@gmail.com>
2025-04-07 14:04:19 +0300

    ref

--- test.py
+++ test.py
@@ -1,10 +1,39 @@
-counter = 23
-cell_to_wid = [i for i in range(0, counter)]
-col_count = 5
+import os
 
-col = len(cell_to_wid) % col_count
-row = len(cell_to_wid) // col_count
 
-print(row, col)
+def get_destination_paths_scandir(src_dir, dest_dir):
+    stack = [src_dir]
+    file_paths = []
 
-print(len(cell_to_wid))
+    while stack:
+        current_fir = stack.pop()
+        for entry in os.scandir(current_fir):
+            if entry.is_dir():
+                stack.append(entry.path)
+            else:
+                # Получаем родительскую директорию исходного пути
+                # В данном случае это путь к директории, в которой находится исходная папка src_dir
+                parent = os.path.dirname(src_dir)
+                
+                # Получаем относительный путь от исходной директории до текущего файла
+                # Этот шаг нужен, чтобы понять, как файл "расположен" относительно папки src_dir
+                rel_path = os.path.relpath(entry.path, parent)
+                
+                # Формируем полный путь для назначения
+                # Здесь мы соединяем путь назначения (dest_dir) с относительным путем,
+                # полученным в предыдущем шаге, чтобы сохранить структуру директорий
+                full_dest = os.path.join(dest_dir, rel_path)
+                file_paths.append(full_dest)
+
+
+    # Возвращаем список путей назначения
+    return file_paths
+
+
+
+src = "/Users/Loshkarev/Desktop/TEST IMAGES"
+dest = "/Users/Loshkarev/Downloads"
+
+paths = get_destination_paths_scandir(src, dest)
+# for i in paths:
+#     print(i)
