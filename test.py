a4263c2dfbd1b28422ae690c079b9fa8f32ed21c
Evgeny Loshkarev <evlosh@gmail.com>
2025-04-14 11:14:13 +0300

    ref

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
 import os
+from utils import Utils
+from cfg import Static
+
+src = "/Volumes/Shares-1/Studio/MIUZ/Photo/Art/Raw/2025/04 - Апрель/2025-04-02 21-02-01.tif"
 
 
-src = "/Applications"
+class Test:
+    @classmethod
+    def find_new_volume(cls, path: str):
+        # Устраняет проблему с изменяющимся префиксом пути к сетевому диску,
+        # например:
+        #   /Volumes/Shares/Studio/MIUZ/file.txt
+        #   /Volumes/Shares-1/Studio/MIUZ/file.txt
+        # Приводит путь к универсальному виду и ищет актуальный том, в котором существует файл.
+        path = Utils.normalize_slash(path)
+        splited = path.split(os.sep)[3:]
+        path = os.path.join(os.sep, *splited)
 
-@classmethod
-def get_applications():
-    for i in os.scandir(src):
-        if i.name.endswith((".APP", ".app")):
-            continue
-        elif i.is_dir():
-            for i in os.scandir(i.path):
-                if i.name.endswith((".APP", ".app")):
-                    print(i.name)
+        for entry in os.scandir(os.sep + Static.VOLUMES):
+            new_path = entry.path + path
+            if os.path.exists(new_path):
+                return new_path
+        return None
+
+
+
+a = Test.find_new_volume(src)
+print(a)
--- utils.py
+++ utils.py
@@ -411,6 +411,25 @@ class Utils(Pixmap, ReadImage, ImgConvert):
         return path
     
     @classmethod
+    def find_new_volume(cls, path: str):
+        """
+        Устраняет проблему с изменяющимся префиксом пути к сетевому диску,
+        например:   
+        /Volumes/Shares/Studio/MIUZ/file.txt    
+        /Volumes/Shares-1/Studio/MIUZ/file.txt  
+        Приводит путь к универсальному виду и ищет актуальный том, в котором существует файл.
+        """
+        path = Utils.normalize_slash(path)
+        splited = path.split(os.sep)[3:]
+        path = os.path.join(os.sep, *splited)
+
+        for entry in os.scandir(os.sep + Static.VOLUMES):
+            new_path = entry.path + path
+            if os.path.exists(new_path):
+                return new_path
+        return None
+
+    @classmethod
     def get_system_volume(cls):
         """
         Получает путь к системному диску /Volumes/Macintosh HD (или иное имя)
