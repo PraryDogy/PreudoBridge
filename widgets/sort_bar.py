31d179d5a87859f2d3fb422ddaa8cacf1f78f12e
Evgeny Loshkarev <evlosh@gmail.com>
2025-04-16 10:47:09 +0300

    ref

--- widgets/sort_bar.py
+++ widgets/sort_bar.py
@@ -1,21 +1,18 @@
 import os
 import subprocess
 
-from PyQt5.QtCore import (QMimeData, QObject, QPoint, Qt, QTimer, QUrl,
-                          pyqtSignal)
-from PyQt5.QtGui import (QContextMenuEvent, QDrag, QKeyEvent, QMouseEvent,
-                         QPixmap)
-from PyQt5.QtWidgets import (QAction, QApplication, QFrame, QHBoxLayout,
-                             QLabel, QPushButton, QVBoxLayout, QWidget)
+from PyQt5.QtCore import QObject, QPoint, Qt, pyqtSignal
+from PyQt5.QtGui import QKeyEvent, QMouseEvent
+from PyQt5.QtWidgets import (QHBoxLayout, QLabel, QPushButton, QVBoxLayout,
+                             QWidget)
 
 from cfg import Dynamic, Static, ThumbData
 from database import ORDER_DICT
-from utils import PathFinder, URunnable, UThreadPool, Utils
+from utils import URunnable, UThreadPool, Utils
 
 from ._base_widgets import (MinMaxDisabledWin, UFrame, ULineEdit, UMenu,
                             USlider, USvgSqareWidget)
-from .actions import CopyPath, Info, RevealInFinder, SortMenu, View
-from .info_win import InfoWin
+from .actions import SortMenu
 
 SORT_T = "Сортировка"
 TOTAL_T = "Всего"
@@ -27,6 +24,119 @@ FINDER_T = "Finder"
 GO_PLACEGOLDER = "Вставьте путь к файлу/папке"
 ARROW_RIGHT = " \U0000203A" # ›
 GO_LINE_EDIT_W = 270
+
+
+class PathFinder:
+
+    @classmethod
+    def get_result(cls, path: str) -> str | None:
+        path = path.strip()
+        path = path.replace("\\", os.sep)
+        path = path.strip("'").strip('"') # кавычки
+        path = Utils.normalize_slash(path)
+
+        # если это локальный путь начинающийся с /Users/Username, то меняем его
+        # на /Volumes/Macintosh HD/Users/Username
+        path = Utils.add_system_volume(path)
+
+        if not path:
+            return None
+
+        splited = [i for i in path.split(os.sep) if i]
+        volumes = [i.path for i in os.scandir(os.sep + Static.VOLUMES)]
+
+        # см. аннотацию add_to_start
+        paths = cls.add_to_start(splited_path=splited, volumes=volumes)
+        res = cls.check_for_exists(paths=paths)
+
+        if res in volumes:
+            return None
+
+        elif res:
+            return res
+        
+        else:
+            # см. аннотацию метода del_from_end
+            paths = [
+                ended_path
+                for path_ in paths
+                for ended_path in cls.del_from_end(path=path_)
+            ]
+
+            paths.sort(key=len, reverse=True)
+            res = cls.check_for_exists(paths=paths)
+
+            if res in volumes:
+                return None
+            
+            elif res:
+                return res
+    
+    @classmethod
+    def add_to_start(cls, splited_path: list, volumes: list[str]) -> list[str]:
+        """
+        Пример:
+        >>> splited_path = ["Volumes", "Shares-1", "Studio", "MIUZ", "Photo", "Art", "Raw", "2025"]
+        >>> volumes = ["/Volumes/Shares", "/Volumes/Shares-1"]
+        [
+            '/Volumes/Shares/Studio/MIUZ/Photo/Art/Raw/2025',
+            '/Volumes/Shares/MIUZ/Photo/Art/Raw/2025',
+            '/Volumes/Shares/Photo/Art/Raw/2025',
+            '/Volumes/Shares/Art/Raw/2025',
+            '/Volumes/Shares/Raw/2025',
+            '/Volumes/Shares/2025',
+            ...
+            '/Volumes/Shares-1/Studio/MIUZ/Photo/Art/Raw/2025',
+            '/Volumes/Shares-1/MIUZ/Photo/Art/Raw/2025',
+            '/Volumes/Shares-1/Photo/Art/Raw/2025',
+            ...
+        ]
+        """
+        new_paths = []
+
+        for vol in volumes:
+
+            splited_path_copy = splited_path.copy()
+            while len(splited_path_copy) > 0:
+
+                new = vol + os.sep + os.path.join(*splited_path_copy)
+                new_paths.append(new)
+                splited_path_copy.pop(0)
+
+        new_paths.sort(key=len, reverse=True)
+        return new_paths
+    
+    @classmethod
+    def check_for_exists(cls, paths: list[str]) -> str | None:
+        for i in paths:
+            if os.path.exists(i):
+                return i
+        return None
+    
+    @classmethod
+    def del_from_end(cls, path: str) -> list[str]:
+        """
+        Пример:
+        >>> path: "/sbc01/Shares/Studio/MIUZ/Photo/Art/Raw/2025"
+        [
+            "/sbc01/Shares/Studio/MIUZ/Photo/Art/Raw/2025",
+            "/sbc01/Shares/Studio/MIUZ/Photo/Art/Raw",
+            "/sbc01/Shares/Studio/MIUZ/Photo/Art",
+            "/sbc01/Shares/Studio/MIUZ/Photo",
+            "/sbc01/Shares/Studio/MIUZ",
+            "/sbc01/Shares/Studio",
+            "/sbc01/Shares",
+            "/sbc01",
+        ]
+        """
+        new_paths = []
+
+        while path != os.sep:
+            new_paths.append(path)
+            path, _ = os.path.split(path)
+
+        return new_paths
+    
 
 class WorkerSignals(QObject):
     finished_ = pyqtSignal(str)
