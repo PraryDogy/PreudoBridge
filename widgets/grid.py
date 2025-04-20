024a2185adf48138ad4a353fd7f209db4630306c
Morkik <evlosh@gmail.com>
2025-04-20 22:50:43 +0300

    уа

--- widgets/grid.py
+++ widgets/grid.py
@@ -776,19 +776,26 @@ class Grid(UScrollArea):
         Испускет сигнал принудительной загрузки изображений для скопированных виджетов.
         """
         for dir in urls:
+            # если url есть в списке path to wid, то удаляем информацию о виджете
+            # и сам виджет из сетки
             if dir in self.path_to_wid:
                 wid = self.remove_widget_data(dir)
                 if wid:
                     wid.deleteLater()
+            # инициируем новый виджет
             wid = Thumb(dir)
             wid.setup_attrs()
             wid.setup_child_widgets()
             wid.set_no_frame()
             wid.set_svg_icon(Utils.get_generic_icon_path(wid.type_))
+            # ищем последнюю строку и столбец в cell to wid
+            # прибавляем +1 к строке и столбец:
+            # сколько прибавлять неважно, так как при перетасовке установится
+            # правильное значение строки и столбца
             row, col = list(self.cell_to_wid.keys())[-1]
             new_row, new_col = row + 1, col + 1
+            self.add_widget_data(wid, new_row, new_col)к
             self.grid_layout.addWidget(wid, new_row, new_col)
-            self.add_widget_data(wid, new_row, new_col)
         self.order_()
         self.rearrange()
         self.force_load_images_sig.emit(urls)
