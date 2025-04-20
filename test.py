48449faec3f8f7ea2d4893fb728304bb09604bd9
Morkik <evlosh@gmail.com>
2025-04-20 12:28:38 +0300

    ref

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-import os
-
-
-src = "/Volumes/Shares-1/Studio/MIUZ/Photo/Art/Raw/2025/04 - Апрель/Серьги для красного цветка"
-
-from utils import Utils
-test = os.stat(src)
-print(Utils.get_f_date(test.st_mtime))
-print(Utils.get_f_date(test.st_birthtime))
+a = 70
