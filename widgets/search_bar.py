f03338cbf387930e1d24efc7884a3e24a0cac134
Evgeny Loshkarev <evlosh@gmail.com>
2025-04-22 17:31:46 +0300

    ref

--- widgets/search_bar.py
+++ widgets/search_bar.py
@@ -6,6 +6,8 @@ from cfg import Dynamic
 
 
 class SearchBar(QFrame):
+    start_new_search = pyqtSignal()
+
     def __init__(self):
         super().__init__()
         self.setFixedHeight(40)
@@ -16,13 +18,14 @@ class SearchBar(QFrame):
         self.setLayout(h_lay)
 
         self.checkbox = QCheckBox(" Точное соответствие")
-        self.checkbox.setChecked(Dynamic.exactly_search)
         self.checkbox.stateChanged.connect(self.on_state_change)
         h_lay.addWidget(self.checkbox)
 
     def on_state_change(self, value: int):
-        data = {
-            0: False,
-            2: True
-        }
+        data = {0: False, 2: True}
         Dynamic.exactly_search = data.get(value)
+        self.start_new_search.emit()
+
+    def show(self):
+        self.checkbox.setChecked(Dynamic.exactly_search)
+        return super().show()
