356b728dc4f865e1acc6ec3ca1234f913b088a80
Evgeny Loshkarev <evlosh@gmail.com>
2025-04-28 10:10:08 +0300

    ref

--- widgets/main_win.py
+++ widgets/main_win.py
@@ -108,10 +108,10 @@ class MainWin(WinBase):
         self.setMinimumSize(MainWin.min_width_, MainWin.min_height_)
         self.resize(MainWin.width_, MainWin.height_)
         if dir:
-            self.main_dir = dir
+            self.main_dir: str = dir
         else:
-            self.main_dir = os.path.expanduser("~/Downloads")
-            self.main_dir = Utils.add_system_volume(self.main_dir)
+            self.main_dir: str = os.path.expanduser("~/Downloads")
+            self.main_dir: str = Utils.add_system_volume(self.main_dir)
         self.main_win_list: list[MainWin] = []
         # индекс 0 просмотр сеткой, индекс 1 просмотр списком
         self.view_index = 0
