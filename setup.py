b25fefe619cefe977879e14bf3bae77527aea0b9
Evgeny Loshkarev <evlosh@gmail.com>
2025-06-06 12:15:59 +0300

    ref

--- setup.py
+++ setup.py
@@ -89,7 +89,8 @@ PY2APP = "py2app" # DON'T CHANGE IT
 
 OPTIONS = {
     "iconfile": ICON_PATH,
-    "packages": ["PIL"],
+    "packages": ["PIL", "numpy"],
+    "excludes": ["setuptools"],
     "plist": {
         "CFBundleName": APP_NAME,
         "CFBundleShortVersionString": APP_VER,
