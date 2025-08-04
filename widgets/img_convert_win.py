d42c0a0d78b206d70dca4fbf5b12e62bcaeb225c
Evgeny Loshkarev <evlosh@gmail.com>
2025-08-04 11:58:57 +0300

    ref

--- widgets/img_convert_win.py
+++ widgets/img_convert_win.py
@@ -12,13 +12,18 @@ class ImgConvertWin(ProgressbarWin):
     def __init__(self, urls: list[str]):
         super().__init__(self.title_text, Static.COPY_FILES_SVG)
         self.progressbar.setMinimum(0)
+        self.urls = urls
 
         if urls:
             self.img_task = ImgConvertTask(urls)
             self.img_task.signals_.set_progress_len.connect(lambda value: self.progressbar.setMaximum(value))
-            self.img_task.signals_.progress_value.connect(lambda value: self.progressbar.setValue(value))
+            self.img_task.signals_.progress_value.connect(lambda value: self.set_value_cmd(value))
             self.img_task.signals_.finished_.connect(lambda urls:self.finished_cmd(urls))
             UThreadPool.start(self.img_task)
+
+    def set_value_cmd(self, value: int):
+        self.above_label.setText(f"{value} из {len(self.urls)}")
+        self.progressbar.setValue(value)
 
     def finished_cmd(self, urls: list[str]):
         subprocess.run(["osascript", Static.REVEAL_SCPT] + urls)
