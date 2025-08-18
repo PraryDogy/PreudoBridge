ae154b7a8608f74f76cd11e257cdd72c4f887dd8
Evgeny Loshkarev <evlosh@gmail.com>
2025-08-18 11:55:52 +0300

    ref

--- cfg.py
+++ cfg.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 
 class Static:
     APP_NAME = "PreudoBridge"
-    APP_VER = 2.8
+    APP_VER = 2.9
 
     USER_SETTINGS_DIR = os.path.expanduser('~/Library/Application Support')
     APP_SUPPORT_APP = os.path.join(USER_SETTINGS_DIR, APP_NAME)
