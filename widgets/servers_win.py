b82b5e37d61f0b73c6a26f695d440e20c27159c7
Evgeny Loshkarev <evlosh@gmail.com>
2025-10-28 10:13:41 +0300

    ref

--- widgets/servers_win.py
+++ widgets/servers_win.py
@@ -1,11 +1,12 @@
 import json
 import os
+import subprocess
 
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QContextMenuEvent, QKeyEvent
 from PyQt5.QtWidgets import (QAction, QGraphicsOpacityEffect, QGridLayout,
-                             QHBoxLayout, QLabel, QSpacerItem, QVBoxLayout,
-                             QWidget)
+                             QHBoxLayout, QLabel, QPushButton, QSpacerItem,
+                             QVBoxLayout, QWidget)
 
 from cfg import Static
 from system.items import BaseItem
@@ -17,50 +18,67 @@ from .actions import CopyText, RevealInFinder
 
 
 class ServerLoginWidget(QWidget):
+    placeholder_text = "Сервер, логин, пароль"
+
     def __init__(self, server: str, login: str, pass_: str):
         super().__init__()
 
         main_lay = QHBoxLayout(self)
         main_lay.setContentsMargins(0, 0, 0, 0)
-        main_lay.setSpacing(0)
 
-        server_wid = ULineEdit()
-        login_wid = ULineEdit()
-        pass_wid = ULineEdit()
+        combo_wid = ULineEdit()
+        if server:
+            combo_wid.setText(f"{server}, {login}, {pass_}")
+        combo_wid.setPlaceholderText(self.placeholder_text)
 
-        server_wid.setText(server)
-        login_wid.setText(login)
-        pass_wid.setText(pass_)
-
-        main_lay.addWidget(server_wid)
-        main_lay.addWidget(login_wid)
-        main_lay.addWidget(pass_wid)
+        main_lay.addWidget(combo_wid)
 
     def get_data(self):
-        return (
-            i.text()
-            for i in self.findChildren(ULineEdit)
-        )
+        return [
+            i.strip()
+            for i in self.findChild(ULineEdit).text().split(",")
+        ]
 
 
 class ServersWin(MinMaxDisabledWin):
     title_text = "Подключение к серверу"
+    connect_text = "Подкл."
+    cancel_text = "Отмена"
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle(self.title_text)
         self.set_modality()
+        self.setFixedWidth(300)
         self.central_layout = QVBoxLayout()
         self.central_layout.setContentsMargins(5, 5, 5, 5)
-        self.central_layout.setSpacing(0)
+        self.central_layout.setSpacing(10)
         self.setLayout(self.central_layout)
         self.data: list[tuple] = {}
         self.empty_data = ("", "", "")
         self.init_data()
         for i in self.data:
-            self.init_ui(i)
-        self.init_ui(self.empty_data)
+            self.init_servers(i)
+        self.init_servers(self.empty_data)
+
+        btn_layout = QHBoxLayout()
+        btn_layout.setContentsMargins(0, 0, 0, 0)
+        btn_layout.setSpacing(10)
+        btn_connect = QPushButton(self.connect_text)
+        btn_cancel = QPushButton(self.cancel_text)
+        btn_connect.clicked.connect(self.connect_cmd)
+        btn_cancel.clicked.connect(self.deleteLater)
+        btn_connect.setFixedWidth(90)
+        btn_cancel.setFixedWidth(90)
+        btn_layout.addStretch()
+        btn_layout.addWidget(btn_connect)
+        btn_layout.addWidget(btn_cancel)
+        btn_layout.addStretch()
+
+        self.central_layout.addLayout(btn_layout)
+
         self.adjustSize()
+        self.setFocus()
 
     def init_data(self):
         json_file = os.path.join(Static.APP_SUPPORT, "servers.json")
@@ -68,7 +86,23 @@ class ServersWin(MinMaxDisabledWin):
             with open(json_file, "r", encoding="utf-8") as file:
                 self.data = json.load(file.read())
 
-    def init_ui(self, server_data: tuple):
+    def init_servers(self, server_data: tuple):
         server, login, pass_ = server_data
         main_wid = ServerLoginWidget(server, login, pass_)
         self.central_layout.addWidget(main_wid)
+
+    def connect_cmd(self):
+
+        def open_smb(data: list[str]):
+            if data and len(data) == 3:
+                server, login, pass_ = data
+                cmd = f"smb://{login}:{pass_}@{server}"
+                subprocess.run(["open", cmd])
+                return cmd
+
+        for i in self.findChildren(ServerLoginWidget):
+            open_smb(i.get_data())
+
+    def mouseReleaseEvent(self, a0):
+        self.setFocus()
+        return super().mouseReleaseEvent(a0)
