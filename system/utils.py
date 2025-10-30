d95123cc9a76d173c3a650175bddf59d4f24207c
Evgeny Loshkarev <evlosh@gmail.com>
2025-10-30 11:58:36 +0300

    ref

--- system/utils.py
+++ system/utils.py
@@ -242,17 +242,8 @@ class Utils:
     
     @classmethod
     def qiconed_resize(cls, pixmap: QPixmap, max_side: int) -> QPixmap:
-        if pixmap.isNull():
-            return QPixmap()
-        w, h = pixmap.width(), pixmap.height()
-        if w > h:
-            new_w = max_side
-            new_h = int(h * max_side / w)
-        else:
-            new_h = max_side
-            new_w = int(w * max_side / h)
-        icon = QIcon(pixmap)
-        return icon.pixmap(QSize(new_w, new_h))
+        # return pixmap.scaled(max_side, max_side, aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio, transformMode=Qt.TransformationMode.SmoothTransformation)
+        return QIcon(pixmap).pixmap(QSize(max_side, max_side))
 
     @classmethod
     def pixmap_scale(cls, pixmap: QPixmap, size: int) -> QPixmap:
