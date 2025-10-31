2985e1ae688eb5c740f14dbde7daf1720a7bacff
Evgeny Loshkarev <evlosh@gmail.com>
2025-10-31 10:36:41 +0300

    ref

--- system/shared_utils.py
+++ system/shared_utils.py
@@ -17,36 +17,6 @@ from PIL import Image, ImageOps
 class SharedUtils:
 
     @classmethod
-    def get_apps(cls, app_names: list[str]):
-        """
-        Возвращает на основе имен приложений:
-        - {путь к приложению: имя приложения, ...}
-        """
-
-        def search_dir(directory):
-            try:
-                for entry in os.scandir(directory):
-                    if entry.name.endswith((".app", ".APP")):
-                        name_lower = entry.name.lower()
-                        if any(k in name_lower for k in app_names):
-                            image_apps[entry.path] = entry.name
-                    elif entry.is_dir():
-                        search_dir(entry.path)
-            except PermissionError:
-                pass
-
-        app_dirs = [
-            "/Applications",
-            os.path.expanduser("~/Applications"),
-            "/System/Applications"
-        ]
-        image_apps: dict[str, str] = {}
-        for app_dir in app_dirs:
-            if os.path.exists(app_dir):
-                search_dir(app_dir)
-        return image_apps
-
-    @classmethod
     def get_sys_vol(cls):
         """
         Возвращает путь к системному диску /Volumes/Macintosh HD (или иное имя)
