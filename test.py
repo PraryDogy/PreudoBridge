3704e9eb9a29c3940797712d7a68b21fedf59457
Evgeny Loshkarev <evlosh@gmail.com>
2025-12-17 10:39:49 +0300

    ref

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-from AppKit import NSBitmapImageRep, NSPNGFileType, NSWorkspace
-from PIL import Image
-from io import BytesIO
+import os
 
 
-def test(filepath: str):
-    _ws = NSWorkspace.sharedWorkspace()
-    icon = _ws.iconForFile_(filepath)
-    tiff = icon.TIFFRepresentation()
-    rep = NSBitmapImageRep.imageRepWithData_(tiff)
-    png = rep.representationUsingType_properties_(NSPNGFileType, None)
-    img = Image.open(BytesIO(bytes(png)))
-    img.show()
+src = '/Volumes/Macintosh HD/.file'
+b = os.access(src, 4)
 
-
-src = "/Volumes/shares"
-test(src)
+print(b)
