72a69cfb24788e1dee5437efa690aed2aa3c7352
Evgeny Loshkarev <evlosh@gmail.com>
2025-12-19 10:23:58 +0300

    ref

--- widgets/img_convert_win.py
+++ widgets/img_convert_win.py
@@ -10,6 +10,7 @@ from .progressbar_win import ProgressbarWin
 
 class ImgConvertWin(ProgressbarWin):
     title_text = "Создаю копии jpg"
+    prepairing = "Подготовка..."
 
     def __init__(self, urls: list[str]):
         super().__init__(self.title_text, os.path.join(Static.internal_icons_dir, "files.svg"))
@@ -17,16 +18,18 @@ class ImgConvertWin(ProgressbarWin):
         self.urls = urls
 
         self.cancel_btn.clicked.connect(self.cancel_cmd)
+        self.above_label.setText(self.prepairing)
+        self.below_label.setText(f"0 из {len(urls)}")
 
         if urls:
             self.tsk_ = ToJpegConverter(urls)
             self.tsk_.sigs.finished_.connect(lambda urls:self.finished_cmd(urls))
-            self.progressbar.setMaximum(self.tsk_.total_count)
+            self.progressbar.setMaximum(len(urls))
             UThreadPool.start(self.tsk_)
 
             self.timer_ = QTimer(self)
             self.timer_.timeout.connect(self.update_gui)
-            self.timer_.start(1000)
+            self.timer_.start(500)
 
     def update_gui(self, limit: int = 25):
         if len(self.tsk_.current_filename) > limit:
@@ -39,10 +42,11 @@ class ImgConvertWin(ProgressbarWin):
         self.progressbar.setValue(self.tsk_.current_count)
 
     def finished_cmd(self, urls: list[str]):
-        self.timer_.stop()
-        self.deleteLater()
+        # self.timer_.stop()
+        # self.deleteLater()
+        ...
 
     def cancel_cmd(self):
-        self.tsk_.is_should_run(False)
-        self.timer_.stop()
-        self.deleteLater()
+        self.tsk_.set_should_run(False)
+        # self.timer_.stop()
+        # self.deleteLater()
