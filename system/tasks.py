b14771d53c35543f30a68fd2698c1733ef9d158c
PraryDogy <evlosh@gmail.com>
2025-12-21 23:45:59 +0300

    ек

--- system/tasks.py
+++ system/tasks.py
@@ -488,8 +488,7 @@ class SearchTask(URunnable):
                 insert(base_item, img_array)
             qimage = Utils.qimage_from_array(img_array)
             base_item.qimage = qimage
-            base_item.uti_type, _ = Utils.uti_generator(entry.path)
-        base_item.qimage = QImage()
+            base_item.uti_type = Utils.get_uti_type(entry.path)
         self.sigs.new_widget.emit(base_item)
         QTest.qSleep(SearchTask.new_wid_sleep_ms)
 
