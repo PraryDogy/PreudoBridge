3d117732f75a61ffcba36001909f0f29180e3f1d
Evgeny Loshkarev <evlosh@gmail.com>
2025-12-23 18:48:36 +0300

    ref

--- system/appkit_icon.py
+++ system/appkit_icon.py
@@ -37,11 +37,13 @@ class AppKitIcon:
         вынести в тред потом
 
         """
-        Dynamic.uti_data[uti_filetype] = {}
+        qimage = Utils.scaled(qimage, size)
+        Dynamic.uti_data[uti_filetype] = {"src": qimage}
         for i in Static.image_sizes:
             resized_qimage = Utils.scaled(qimage, i)
             Dynamic.uti_data[uti_filetype][i] = resized_qimage
-        Dynamic.uti_data[uti_filetype]["src"] = Utils.scaled(qimage, size)
+        save_path = os.path.join(Static.external_uti_dir, f"{self.data.uti_type}.png")
+        qimage.save(save_path, "PNG")
 
     def get_uti_bundle(self):
         return NSBundle.bundleWithPath_(self.path).bundleIdentifier()
@@ -49,51 +51,35 @@ class AppKitIcon:
     def worker(self) -> dict[int | str, QImage]:
         type_symlink = "public.symlink"
         type_application = "com.apple.application-bundle"
-        empty_file = "public.data"
+        empty_icon = "public.data"
 
         if self.uti_filetype in Dynamic.uti_data:
             return Dynamic.uti_data[self.uti_filetype]
 
-        if self.uti_filetype == type_symlink:
-            # получаем хеш сумму байтов
-            appkit_hash = self.get_uti_bytes_hash()
-            # если иконка еще не закэширована
-            if appkit_hash not in Dynamic.uti_data:
+        elif self.uti_filetype == type_symlink:
+            self.uti_filetype = self.get_uti_bytes_hash()
+            if self.uti_filetype not in Dynamic.uti_data:
                 bytes_icon = self.get_uti_bytes_img()
                 qimage = QImage()
                 qimage.loadFromData(bytes_icon)
-                # добавляем в хэш все размеры иконки по списку Static.image_sizes
-                # а так же хэш размера 512 на 512 по ключу "src"
-                self.set_uti_data(appkit_hash, qimage)
-                # сохраняем иконку
-                uti_png_icon_path = os.path.join(Static.external_uti_dir, f"{appkit_hash}.png")
-                qimage_for_save: QImage = Dynamic.uti_data[appkit_hash]["src"]
-                qimage_for_save.save(uti_png_icon_path, "PNG")
+                self.set_uti_data(self.uti_filetype, qimage)
         
         elif self.uti_filetype == type_application:
-            bundle = self.get_uti_bundle()
-            if bundle not in Dynamic.uti_data:
+            self.uti_filetype = self.get_uti_bundle()
+            if self.uti_filetype not in Dynamic.uti_data:
                 bytes_icon = self.get_uti_bytes_img()
                 qimage = QImage()
                 qimage.loadFromData(bytes_icon)
-                Utils.set_uti_data(bundle, qimage)
-                uti_png_icon_path = os.path.join(Static.external_uti_dir, f"{bundle}.png")
-                qimage_for_save: QImage = Dynamic.uti_data[bundle]["src"]
-                qimage_for_save.save(uti_png_icon_path, "PNG")
-                qimage.save(uti_png_icon_path, "PNG")
-
-        elif self.uti_filetype is None:
-            self.uti_filetype = empty_file
+                self.set_uti_data(self.uti_filetype, qimage)
 
         elif self.uti_filetype not in Dynamic.uti_data:
-            bytes_icon = Utils.get_uti_bytes_img(self.data.src)
+            bytes_icon = self.get_uti_bytes_img()
             qimage = QImage()
             qimage.loadFromData(bytes_icon)
-            Utils.set_uti_data(self.data.uti_type, qimage)
-            uti_png_icon_path = os.path.join(Static.external_uti_dir, f"{self.data.uti_type}.png")
-            qimage_for_save: QImage = Dynamic.uti_data[self.data.uti_type]["src"]
-            qimage_for_save.save(uti_png_icon_path, "PNG")
-            qimage.save(uti_png_icon_path, "PNG")
+            self.set_uti_data(self.data.uti_type, qimage)
+
+        elif self.uti_filetype is None:
+            self.uti_filetype = empty_icon
 
         try:
             qimage = Dynamic.uti_data[self.data.uti_type][Thumb.current_image_size]
