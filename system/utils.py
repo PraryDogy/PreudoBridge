ed7d963342df4ffebeaff83df31d650495283770
Evgeny Loshkarev <evlosh@gmail.com>
2025-12-23 17:36:48 +0300

    ref

--- system/utils.py
+++ system/utils.py
@@ -249,19 +249,24 @@ class Utils:
         uti_filetype, _ = Utils._ws.typeOfFile_error_(filepath, None)
         return uti_filetype
     
-    # @classmethod
-    # def get_appkit_bytes_icon(cls, filepath: str):
-    #     icon = Utils._ws.iconForFile_(filepath)
-    #     tiff = icon.TIFFRepresentation()
-    #     rep = NSBitmapImageRep.imageRepWithData_(tiff)
-    #     png = rep.representationUsingType_properties_(NSPNGFileType, None)
-    #     return bytes(png)
+    @classmethod
+    def get_appkit_png_bytes(cls, filepath: str):
+        icon = Utils._ws.iconForFile_(filepath)
+        tiff = icon.TIFFRepresentation()
+        rep = NSBitmapImageRep.imageRepWithData_(tiff)
+        png = rep.representationUsingType_properties_(NSPNGFileType, None)
+        return bytes(png)
+        
+    @classmethod
+    def get_appkit_png_hash(cls, filepath: str):
+        icon = Utils._ws.iconForFile_(filepath)
+        tiff_bytes = icon.TIFFRepresentation()[:-1].tobytes()
+        return hashlib.md5(tiff_bytes).hexdigest()
     
-    # @classmethod
-    # def get_appkit_icon_hash(cls, filepath: str):
-    #     icon = Utils._ws.iconForFile_(filepath)
-    #     tiff_bytes = icon.TIFFRepresentation()[:-1].tobytes()
-    #     return hashlib.md5(tiff_bytes).hexdigest()
+    @classmethod
+    def get_appkit_error_icon(cls):
+        uti_filetype_ = "public.data"
+        return "public.data", Dynamic.uti_data[uti_filetype_]
 
     @classmethod
     def uti_generator(cls, filepath: str, size: int = 512):
@@ -271,46 +276,30 @@ class Utils:
         словарик с QImage, соответвующий всем размерам из Static.image_sizes
         """
 
-        def get_bytes_icon(filepath: str):
-            icon = Utils._ws.iconForFile_(filepath)
-            tiff = icon.TIFFRepresentation()
-            rep = NSBitmapImageRep.imageRepWithData_(tiff)
-            png = rep.representationUsingType_properties_(NSPNGFileType, None)
-            return bytes(png)
-        
-        def get_errored_icon():
-            uti_filetype_ = "public.data"
-            return uti_filetype_, Dynamic.uti_data[uti_filetype_]
-        
         def set_uti_data(uti_filetype: str, qimage: QImage):
             Dynamic.uti_data[uti_filetype] = {}
             for i in Static.image_sizes:
                 resized_qimage = Utils.scaled(qimage, i)
                 Dynamic.uti_data[uti_filetype][i] = resized_qimage
 
-        def get_symlink_bytes(filepath: str):
-            icon = Utils._ws.iconForFile_(filepath)
-            tiff_bytes = icon.TIFFRepresentation()[:-1].tobytes()
-            return hashlib.md5(tiff_bytes).hexdigest()
-
-        uti_filetype, _ = Utils._ws.typeOfFile_error_(filepath, None)
+        uti_filetype = cls.get_appkit_uti_type(filepath)
 
         if uti_filetype == "public.symlink":
-            symlink_bytes = get_symlink_bytes(filepath)
-            if symlink_bytes in Dynamic.uti_data:
-                return symlink_bytes, Dynamic.uti_data[symlink_bytes]
+            appkit_hash = cls.get_appkit_png_hash(filepath)
+            if appkit_hash in Dynamic.uti_data:
+                return appkit_hash, Dynamic.uti_data[appkit_hash]
             
-            bytes_icon = get_bytes_icon(filepath)
+            bytes_icon = cls.get_appkit_png_bytes(filepath)
             qimage = QImage()
             qimage.loadFromData(bytes_icon)
-            set_uti_data(symlink_bytes, qimage)
+            set_uti_data(appkit_hash, qimage)
 
-            uti_png_icon_path = os.path.join(Static.external_uti_dir, f"{symlink_bytes}.png")
+            uti_png_icon_path = os.path.join(Static.external_uti_dir, f"{appkit_hash}.png")
             if not os.path.exists(uti_png_icon_path):
                 qimage = QImage.fromData(bytes_icon)
                 qimage = Utils.scaled(qimage, size)
                 qimage.save(uti_png_icon_path, "PNG")
-            return symlink_bytes, Dynamic.uti_data[symlink_bytes]
+            return appkit_hash, Dynamic.uti_data[appkit_hash]
         
         if uti_filetype == "com.apple.application-bundle":
             bundle = NSBundle.bundleWithPath_(filepath).bundleIdentifier()
@@ -318,7 +307,7 @@ class Utils:
             if bundle in Dynamic.uti_data:
                 return bundle, Dynamic.uti_data[bundle]
 
-            bytes_icon = get_bytes_icon(filepath)
+            bytes_icon = cls.get_appkit_png_bytes(filepath)
             qimage = QImage()
             qimage.loadFromData(bytes_icon)
             set_uti_data(bundle, qimage)
@@ -332,7 +321,7 @@ class Utils:
             return bundle, Dynamic.uti_data[bundle]
 
         if not uti_filetype:
-            return get_errored_icon()
+            return cls.get_appkit_error_icon()
 
         if uti_filetype in Dynamic.uti_data:
             return uti_filetype, Dynamic.uti_data[uti_filetype]
@@ -340,7 +329,7 @@ class Utils:
         uti_png_icon_path = os.path.join(Static.external_uti_dir, f"{uti_filetype}.png")
 
         if not os.path.exists(uti_png_icon_path):
-            bytes_icon = get_bytes_icon(filepath)
+            bytes_icon = cls.get_appkit_png_bytes(filepath)
 
             qimage = QImage.fromData(bytes_icon)
             qimage = Utils.scaled(qimage, size)
@@ -348,7 +337,7 @@ class Utils:
 
         qimage = QImage(uti_png_icon_path)
         if qimage.isNull():
-            return get_errored_icon()
+            return cls.get_appkit_error_icon()
 
         set_uti_data(uti_filetype, qimage)
         return uti_filetype, Dynamic.uti_data[uti_filetype]
