d4a933d1535c38d151176c6637a9ea408f38126b
Evgeny Loshkarev <evlosh@gmail.com>
2025-12-25 10:30:04 +0300

    ref

--- system/tasks.py
+++ system/tasks.py
@@ -497,95 +497,65 @@ class SearchTask(URunnable):
         QTest.qSleep(SearchTask.new_wid_sleep_ms)
 
 
-class FinderItemsLoader(URunnable):
+# class FinderItemsLoader(URunnable):
 
-    class Sigs(QObject):
-        finished_ = pyqtSignal(dict)
+#     class Sigs(QObject):
+#         finished_ = pyqtSignal(dict)
 
-    hidden_syms: tuple[str] = ()
-    sql_errors = (IntegrityError, OperationalError)
+#     hidden_syms: tuple[str] = ()
+#     sql_errors = (IntegrityError, OperationalError)
 
-    def __init__(self, main_win_item: MainWinItem, sort_item: SortItem):
-        """
-        Вернет словарик
-        {"path": str путь,  "data_items": [DataItem, DataItem, ...]
-        """
+#     def __init__(self, main_win_item: MainWinItem, sort_item: SortItem):
+#         """
+#         Вернет словарик
+#         {"path": str путь,  "data_items": [DataItem, DataItem, ...]
+#         """
 
-        super().__init__()
-        self.sigs = FinderItemsLoader.Sigs()
-        self.sort_item = sort_item
-        self.main_win_item = main_win_item
+#         super().__init__()
+#         self.sigs = FinderItemsLoader.Sigs()
+#         self.sort_item = sort_item
+#         self.main_win_item = main_win_item
 
-        self.finder_items: dict[str, DataItem] = {}
-        self.db_items: dict[str, int] = {}
+#         self.finder_items: dict[str, DataItem] = {}
+#         self.db_items: dict[str, int] = {}
 
-        if not JsonData.show_hidden:
-            self.hidden_syms = Static.hidden_symbols
+#         if not JsonData.show_hidden:
+#             self.hidden_syms = Static.hidden_symbols
 
-    def task(self):
-        try:
-            self._task()
-        except Exception as e:
-            print("tasks, FinderItems error", e)
-            import traceback
-            print(traceback.format_exc())
+#     def task(self):
+#         try:
+#             self._task()
+#         except Exception as e:
+#             print("tasks, FinderItems error", e)
+#             import traceback
+#             print(traceback.format_exc())
 
-    def _task(self):
-        items: list[DataItem] = []
-        path_finder = PathFinder(self.main_win_item.main_dir)
-        fixed_path = path_finder.get_result()
+#     def _task(self):
+#         items: list[DataItem] = []
+#         path_finder = PathFinder(self.main_win_item.main_dir)
+#         fixed_path = path_finder.get_result()
 
-        if fixed_path is None:
-            self.sigs.finished_.emit({"path": None, "data_items": items})
-            return
+#         if fixed_path is None:
+#             self.sigs.finished_.emit({"path": None, "data_items": items})
+#             return
 
-        for i, path in enumerate(self._get_paths(fixed_path)):
-            item = DataItem(path)
-            item.set_properties()
-            items.append(item)
+#         for i, path in enumerate(self._get_paths(fixed_path)):
+#             item = DataItem(path)
+#             item.set_properties()
+#             items.append(item)
 
-        items = DataItem.sort_(items, self.sort_item)
-        self.sigs.finished_.emit({"path": fixed_path, "data_items": items})
+#         items = DataItem.sort_(items, self.sort_item)
+#         self.sigs.finished_.emit({"path": fixed_path, "data_items": items})
 
-    def _get_paths(self, fixed_path: str):
-        for entry in os.scandir(fixed_path):
-            if entry.name.startswith(self.hidden_syms):
-                continue
-            if not os.access(entry.path, 4):
-                print("tasks, finder items loader, get paths, access deined", entry.path)
-                continue
-            yield entry.path
+#     def _get_paths(self, fixed_path: str):
+#         for entry in os.scandir(fixed_path):
+#             if entry.name.startswith(self.hidden_syms):
+#                 continue
+#             if not os.access(entry.path, 4):
+#                 print("tasks, finder items loader, get paths, access deined", entry.path)
+#                 continue
+            # yield entry.path
 
-
-class FinderUrlsLoader(URunnable):
-
-    class Sigs(QObject):
-        finished_ = pyqtSignal(list)
-
-    hidden_syms: tuple[str] = ()
-    sql_errors = (IntegrityError, OperationalError)
-
-    def __init__(self, main_win_item: MainWinItem):
-        super().__init__()
-        self.sigs = FinderItemsLoader.Sigs()
-        self.main_win_item = main_win_item
-
-    def task(self):
-        try:
-            self.sigs.finished_.emit(self._task())
-        except Exception as e:
-            print("tasks, FinderUrls error", e)
-            # import traceback
-            # print(traceback.format_exc())
-
-    def _task(self):
-        hidden_syms = () if JsonData.show_hidden else Static.hidden_symbols
-        return [
-            i.path
-            for i in os.scandir(self.main_win_item.main_dir)
-            if not i.name.startswith(hidden_syms)
-        ]
-        
 
 class DbItemsLoader(URunnable):
 
