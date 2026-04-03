529fe159747e31e11ccf6c7292acd3ad1bf5d074
PraryDogy <evlosh@gmail.com>
2026-04-03 10:20:01 +0300

    ref

--- widgets/remove_files_win.py
+++ widgets/remove_files_win.py
@@ -8,63 +8,27 @@ from system.items import MainWinItem
 from system.tasks import FileRemover, UThreadPool
 
 from ._base_widgets import MinMaxDisabledWin, SmallBtn, USvgSqareWidget
+from .warn_win import ConfirmWindow
 
 
-class RemoveFilesWin(MinMaxDisabledWin):
+class RemoveFilesWin(ConfirmWindow):
     finished_ = pyqtSignal(list)
     remove_perm = "Удалить безвозвратно"
     move_to_trash = "Переместить в корзину"
-    ok_text = "Ок"
-    cancel_text = "Отмена"
-    title_text = "Внимание!"
-    svg_size = 50
 
     def __init__(self, main_win_item: MainWinItem, urls: list[str]):
-        super().__init__()
-        self.setWindowTitle(RemoveFilesWin.title_text)
-        self.set_modality()
-        self.urls = urls
-        self.main_win_item = main_win_item
-
-        v_lay = QVBoxLayout()
-        v_lay.setContentsMargins(10, 10, 10, 10)
-        self.centralWidget().setLayout(v_lay)
-
-        first_row_wid = QWidget()
-        v_lay.addWidget(first_row_wid)
-        first_row_lay = QHBoxLayout()
-        first_row_lay.setContentsMargins(0, 0, 0, 0)
-        first_row_wid.setLayout(first_row_lay)
-
-        warn = USvgSqareWidget(os.path.join(Static.internal_images_dir, "warning.svg"), RemoveFilesWin.svg_size)
-        first_row_lay.addWidget(warn)
 
         if Dynamic.sys_vol in urls[0]:
             t = f"{RemoveFilesWin.move_to_trash} ({len(urls)})?"
         else:
             t = f"{RemoveFilesWin.remove_perm} ({len(urls)})?"
-        question = QLabel(text=t)
-        first_row_lay.addWidget(question)
 
-        h_wid = QWidget()
-        v_lay.addWidget(h_wid)
-        h_lay = QHBoxLayout()
-        h_lay.setContentsMargins(0, 0, 0, 0)
-        h_lay.setSpacing(10)
-        h_lay.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        h_wid.setLayout(h_lay)
+        super().__init__(text=t)
+        self.urls = urls
+        self.main_win_item = main_win_item
 
-        ok_btn = SmallBtn(RemoveFilesWin.ok_text)
-        ok_btn.clicked.connect(self.cmd_)
-        ok_btn.setFixedWidth(90)
-        h_lay.addWidget(ok_btn)
-
-        can_btn = SmallBtn(RemoveFilesWin.cancel_text)
-        can_btn.clicked.connect(self.deleteLater)
-        can_btn.setFixedWidth(90)
-        h_lay.addWidget(can_btn)
-
-        self.adjustSize()
+        self.ok_btn.clicked.connect(self.cmd_)
+        self.cancel_btn.clicked.connect(self.deleteLater)
 
     def cmd_(self, *args):
         self.hide()
